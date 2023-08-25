6414e548451dff6fac9486304f21533de5dcc96e
Lucas Borges Marra <lucasmarra63@gmail.com>
2023-08-24 22:43:01 -0300

    Identação do código

--- js/script.js
+++ js/script.js
@@ -1,46 +1,43 @@
-const marioJump = document.querySelector('.mario');
-const pipe = document.querySelector('.pipe');
-const score = document.querySelector('.score');
+const marioJump = document.querySelector(".mario");
+const pipe = document.querySelector(".pipe");
+const score = document.querySelector(".score");
 let count = 0;
 
 const jump = () => {
-    marioJump.classList.add('jump');
-    setTimeout(() => {
-        marioJump.classList.remove('jump');
-    }, 500);
-}
+  marioJump.classList.add("jump");
+  setTimeout(() => {
+    marioJump.classList.remove("jump");
+  }, 500);
+};
 
 const loop = setInterval(() => {
-    const pipePosition = pipe.offsetLeft;
-    const marioPosition = +window.getComputedStyle(marioJump).bottom.replace('px', ' ');
+  const pipePosition = pipe.offsetLeft;
+  const marioPosition = +window
+    .getComputedStyle(marioJump)
+    .bottom.replace("px", " ");
 
-    if(pipePosition <= 120 && pipePosition > 0 && marioPosition <80){
-        count = -1;
+  if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+    count = -1;
 
-        pipe.style.animation = 'none';
-        pipe.style.left = `${pipePosition}px`;
+    pipe.style.animation = "none";
+    pipe.style.left = `${pipePosition}px`;
 
-        marioJump.style.animation = 'none';
-        marioJump.style.bottom = `${marioPosition}px`;
+    marioJump.style.animation = "none";
+    marioJump.style.bottom = `${marioPosition}px`;
 
-        marioJump.src = './images/game-over.png';
-        marioJump.style.width = '75px';
-        marioJump.style.marginLeft = '50px';
-
-    }
-
-    
+    marioJump.src = "./images/game-over.png";
+    marioJump.style.width = "75px";
+    marioJump.style.marginLeft = "50px";
+  }
 }, 10);
 
-const scoreCount = setInterval(()=> {
-    count++
-    score.innerHTML = `SCORE: ${count}`;
-},15)
+const scoreCount = setInterval(() => {
+  count++;
+  score.innerHTML = `SCORE: ${count}`;
+}, 15);
 
-document.addEventListener('keydown', (e) => {
-    if((e.code === "ArrowUp") | (e.code === "Space")) {
-        jump()
-    }
+document.addEventListener("keydown", (e) => {
+  if ((e.code === "ArrowUp") | (e.code === "Space")) {
+    jump();
+  }
 });
-
-    
